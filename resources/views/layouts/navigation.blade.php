e2b47bd772568493fa11587792248f5c7add6af2
donarazhar <donarazhar@github.com>
2026-07-02 15:02:32 +0700

    style: Change Lihat Portal Publik menu to preview icon in dashboard navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,8 +24,11 @@
                             Dokumen
                         </x-nav-link>
                     @endif
-                    <x-nav-link :href="route('home')" target="_blank">
-                        Lihat Portal Publik
+                    <x-nav-link :href="route('home')" target="_blank" title="Lihat Portal Publik">
+                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
+                        </svg>
                     </x-nav-link>
                 </div>
             </div>
@@ -91,7 +94,13 @@
                 </x-responsive-nav-link>
             @endif
             <x-responsive-nav-link :href="route('home')" target="_blank">
-                Lihat Portal Publik
+                <div class="flex items-center space-x-2">
+                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
+                    </svg>
+                    <span>Lihat Portal Publik</span>
+                </div>
             </x-responsive-nav-link>
         </div>
 
