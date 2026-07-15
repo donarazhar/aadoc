d51cc15ed160a94ecef85b8b97ba5d09970e8b65
donarazhar <donarazhar@gmail.com>
2026-07-15 14:54:30 +0700

    Security Cleanup: Remove utility seeders after database execution

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -46,9 +46,6 @@ class DatabaseSeeder extends Seeder
         // Memanggil seeder lainnya agar datanya ikut masuk
         $this->call([
             GettingStartedSeeder::class,
-            MigrasiKategoriSeeder::class,
-            UpdateArtikelUIUXSeeder::class,
-            CleanupDuplicateArtikelSeeder::class,
         ]);
     }
 }
