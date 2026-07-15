dfcd1528f108b665de5d85c7ccbe72966511aacf
donarazhar <donarazhar@gmail.com>
2026-07-15 08:39:38 +0700

    Add User Manuals for Mobile Parents and Backoffice Roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -47,6 +47,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             GettingStartedSeeder::class,
             PanduanWorkflowDiscountInstallmentSeeder::class,
+            PanduanUserOrangTuaSeeder::class,
+            PanduanUserBackofficeSeeder::class,
         ]);
     }
 }
